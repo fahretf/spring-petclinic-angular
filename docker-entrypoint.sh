ba52c4f9848a70de5891dce134a816f5497f12dd
fahretf <ffijuljani1@etf.unsa.ba>
2025-03-24 15:42:31 +0100

    add check for backend in entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,4 +7,9 @@ echo "};" >> /usr/share/nginx/html/config.js
 BACKEND_URL=${BACKEND_URL:-http://petclinicbe:9966}
 sed -i "s|__BACKEND_URL__|${BACKEND_URL}|g" /etc/nginx/conf.d/default.conf
 
+until nc -z petclinicbe 9966; do
+  echo "Wait for backend to be ready"
+  sleep 1
+done
+
 exec nginx -g "daemon off;"
